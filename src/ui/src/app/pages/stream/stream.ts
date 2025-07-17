de2587d41656d1412d6edc2167997fde51156ace
Stephanie Robles <stephanie.mrobles@yahoo.com.ph>
2025-07-17 12:05:34 +0800

    working frontend skeleton

---
+++ src/ui/src/app/pages/stream/stream.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stream',
+  imports: [],
+  standalone: true,
+  templateUrl: './stream.html',
+  styleUrl: './stream.scss'
+})
+export class Stream {
+
+}
